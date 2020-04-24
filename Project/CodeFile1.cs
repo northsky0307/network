6864d83e2db36577283881a252ec0d2422b825cd
zc <LENOVO@zc>
2020-04-24 17:52:07 +0800

    1

--- Project/CodeFile1.cs
+++ Project/CodeFile1.cs
@@ -4,23 +4,30 @@ using System.Drawing;
 
 namespace Project
 {
+    
     class HelloWord
     {
+        
         public static void Main()
         {
-            Form frm = new Form();
+            //Form1 用户窗口1  Form2  登陆界面
+            //窗口2的引用
+            Form2 form1 = new Form2();
+           // form1.Show();
+            Application.Run(form1);
+            /*Form frm = new Form();
            
             frm.Paint += new PaintEventHandler(frm_Paint); //注册Paint事件处理程序
             frm.Click += new System.EventHandler(frm_Click);//注册Click事件处理程序
             UserControl1 userControl1 = new UserControl1(); //新建按钮
             frm.Controls.Add(userControl1);//添加到窗口
             
-            Application.Run(frm);
+            Application.Run(frm);*/
         }
 
         
         // Click事件处理程序
-        static void frm_Click(object sender, EventArgs e)
+        /*static void frm_Click(object sender, EventArgs e)
         {
             Form frm = (Form)sender;
             //在标题栏显示鼠标单击的位置
@@ -35,6 +42,6 @@ namespace Project
             //g.Clear(Color.Black);
             g.DrawString("Hello Word", frm.Font, Brushes.White, new Point(20, 20));
 
-        }
+        }*/
     }
 }
