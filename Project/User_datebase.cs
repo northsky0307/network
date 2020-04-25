046c33af204d616ab90b63ea90a584165b3cce4b
耀辰 朱 <zhuyaochen01@126.com>
2020-04-25 21:09:04 +0800

    21.08

--- Project/User_datebase.cs
+++ Project/User_datebase.cs
@@ -12,16 +12,25 @@ namespace Project
         private ArrayList User_Information_Index = new ArrayList(); //新建vector
         private Dictionary<string, int> User_Number_Dictionary = new Dictionary<string, int>(); // name对应的num
         private int Database_Size = 0;
-        
-        public int Add_User_Information(string name, string password, bool type = false) //1 已经存在 0 成功 -1异常（user和admin冲突）
+        /// <summary>
+        /// 添加用户信息
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="password"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public int Add_User_Information(string name, string password, bool type = false) //1 已经存在 0 成功 -1异常（user和admin冲突） -2 用户名为空
         {
-
-            if(User_Number_Dictionary.ContainsKey(name))
+            if(name == "")
+            {
+                return -2;
+            }
+            if (User_Number_Dictionary.ContainsKey(name))
             {
                 int num = User_Number_Dictionary[name]; // 得到对应num
 
                 User_Information now_user = (User_Information)User_Information_Index[num];
-                if(type != now_user.Get_Type())
+                if (type != now_user.Get_Type())
                 {
                     System.Console.WriteLine("user and admin conflict");
                     return -1;
@@ -30,12 +39,30 @@ namespace Project
             }
             else
             {
-                User_Information now_user = new User_Information(name, password,type);
+                User_Information now_user = new User_Information(name, password, type);
                 //now_user.Set_Number = Database_Size
                 //(User_Information)User_Information_Index[num];
+                now_user.Set_Number(Database_Size);
+                int num = Database_Size;
+                User_Information_Index[num] = now_user;
+                Database_Size++; ///后移一位
             }
             System.Console.WriteLine(Database_Size);
             return 0;
         }
+        /// <summary>
+        /// 查询用户密码
+        /// </summary>
+        public string Query_User_Information(string name) ///正常返回密码， 异常返回-1
+        {
+            if (User_Number_Dictionary.ContainsKey(name))
+            {
+                User_Information now_user = (User_Information)User_Information_Index[User_Number_Dictionary[name]];
+                return now_user.User_password;
+            }
+            else
+                return "-1";
+        }
+        /// 删除用户
     }
 }
