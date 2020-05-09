3e4c3f878564124ecbcd0f4354842b001fd8c9f6
耀辰 朱 <zhuyaochen01@126.com>
2020-05-09 21:26:32 +0800

    21.26/5/9

--- Project/main.cs
+++ Project/main.cs
@@ -12,20 +12,8 @@ namespace Project
         {
             //Form1 用户窗口1  Form2  登陆界面
             //窗口2的引用
-            string strpath = "..\\..\\read.csv";
-            StreamReader mysr = new StreamReader(strpath, System.Text.Encoding.Default);
-            string str;
-            while ((str = mysr.ReadLine()) != null)
-            {
-                Console.WriteLine(str);
-                string[] now_string = str.Split(',');
-                Rule rule = new Rule(now_string[0], now_string[1], Convert.ToInt32(now_string[2]));
-                Static.rule_index.Add(rule);
-                Rule ttry = (Rule)Static.rule_index[0];
-                System.Console.WriteLine(ttry.query_type);
-
-            }
-            mysr.Close();
+            IOStream my_iostream = new IOStream(); //csv 读写
+            my_iostream.Get_rule(); //读取规则
             /*
                 FileStream fs = new FileStream(strpath, System.IO.FileMode.Create, System.IO.FileAccess.Write);
 
@@ -39,5 +27,7 @@ namespace Project
 
 
         }
+
+        
     }
 }
