b9c62558c5b107bf1dd42dfbdaef54281813611e
耀辰 朱 <zhuyaochen01@126.com>
2020-05-24 21:27:58 +0800

    21.27

--- Project/Sign_in.cs
+++ Project/Sign_in.cs
@@ -214,10 +214,5 @@ namespace Project
         private void textBox10_TextChanged(object sender, EventArgs e)
         {
         }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
     }
 }
