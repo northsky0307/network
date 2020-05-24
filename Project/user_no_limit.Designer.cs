4dd21339fb234e7ae63e19f2a7b0602aee0e5120
耀辰 朱 <zhuyaochen01@126.com>
2020-05-24 22:30:48 +0800

    22.30/5/24

--- Project/user_no_limit.Designer.cs
+++ Project/user_no_limit.Designer.cs
@@ -1,10 +1,7 @@
 ﻿namespace Project
 {
-<<<<<<< HEAD:Project/user_input_error.Designer.cs
-    partial class user_input_error
-=======
+
     partial class user_no_limit
->>>>>>> temp:Project/user_no_limit.Designer.cs
     {
         /// <summary>
         /// Required designer variable.
@@ -36,8 +33,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-<<<<<<< HEAD:Project/user_input_error.Designer.cs
-=======
+
             // label1
             // 
             this.label1.AutoSize = true;
@@ -49,16 +45,12 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "没有访问权限！";
             // 
->>>>>>> temp:Project/user_no_limit.Designer.cs
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.button1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-<<<<<<< HEAD:Project/user_input_error.Designer.cs
-            this.button1.Location = new System.Drawing.Point(173, 144);
-=======
+
             this.button1.Location = new System.Drawing.Point(159, 138);
->>>>>>> temp:Project/user_no_limit.Designer.cs
             this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 29);
@@ -67,24 +59,7 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-<<<<<<< HEAD:Project/user_input_error.Designer.cs
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(75, 78);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(320, 27);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "格式错误！请输入数字！";
-            // 
-            // user_input_error
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(457, 216);
-=======
+
             // user_no_limit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
@@ -92,19 +67,14 @@
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(431, 219);
             this.Controls.Add(this.button1);
->>>>>>> temp:Project/user_no_limit.Designer.cs
+
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-<<<<<<< HEAD:Project/user_input_error.Designer.cs
-            this.Name = "user_input_error";
-            this.Text = "输入错误";
-            this.Load += new System.EventHandler(this.user_input_error_Load);
-=======
+
             this.Name = "user_no_limit";
             this.Text = "无权访问";
             this.Load += new System.EventHandler(this.user_no_limit_Load);
->>>>>>> temp:Project/user_no_limit.Designer.cs
             this.ResumeLayout(false);
             this.PerformLayout();
 
